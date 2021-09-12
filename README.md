6d40efe83799fb7a8c1b927c090b9bd92e2b3d76
Sulaimanov, Suleiman <suleimansulaimanov@my.unt.edu>
2021-09-12 00:54:20 -0500

    created Rect class and now user can move several rectangles.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DragAndDrop
+
+#About
+In this programm you will be able to drag and drop opjects you see, using your gestures.
+
+## Set up the environment
+Run following commands to set up the environment
+- `python3 -m venv venv` create virtual environemnt
+- `source venv/bin/activate` activate virtual environment
+- `pip install cvzone` install cvzone
+- `pip install opencv-python` install cv2
+
+## Run the Code
+To run the code simply type:
+```
+python main.py   
+```
--- main.py
+++ main.py
@@ -3,9 +3,47 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8)
+
 colorR = 255, 0, 255
 
 cx, cy, w, h = 800, 100, 200, 200
+
+class Rect():
+
+    def __init__(self, center, size=[200, 200], color=[255, 0, 255]):
+        self.center = center
+        self.size = size
+        self.color=color
+
+    def update(self, cursor):
+
+        oldSize = self.size
+        oldColor = self.color
+
+        cx, cy = self.center
+        w, h = self.size
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            self.center = cursor
+            self.color = [0, 255, 0]
+            self.size = [220, 220]
+        else:
+            self.size = oldSize
+            self.color = oldColor
+
+
+
+
+    def setView(self, newColor=[255, 0, 255], newSize = [200, 200]):
+        self.color = newColor
+        self.size = newSize
+
+
+
+rect = Rect([150, 150], [200, 200], colorR)
+
+listOfRects = []
+for x in range(5):
+    listOfRects.append(Rect([x * 250 + 150, 150], [200, 200], colorR))
 
 
 while True:
@@ -15,25 +53,35 @@ while True:
     # Find the hand and its landmarks
     hands, img = detector.findHands(img)  # with draw
 
-    cv2.rectangle(img, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), colorR, cv2.FILLED)
-
     if hands:
         # Hand 1
         hand1 = hands[0]
         lmList1 = hand1["lmList"]  # List of 21 Landmark points
 
+        length, _, _ = detector.findDistance(lmList1[8], lmList1[12], img)
 
-        l, _, _ = detector.findDistance(lmList1[8], lmList1[12], img)
-
-        if l <= 35:
+        if length <= 40:
             cursor = lmList1[8]
-            if cx-w//2 < cursor[0] < cx + w//2 and cy-h//2 < cursor[1] < cy+h//2 :
-                colorR = 0, 255, 0
-                cx, cy = cursor
-        else:
-            colorR = 255, 0, 255
+            for r in listOfRects:
+                r.update(cursor)
+
+        # else:
+        #     for r in listOfRects:
+        #         r.setView()
+        #     # rect.setView()
 
 
+
+    for r in listOfRects:
+        cx, cy = r.center
+        w, h = r.size
+        colorR = r.color
+        cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+
+    # cx, cy = rect.center
+    # w, h = rect.size
+    # colorR = rect.color
+    # cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
 
 
     # Display
