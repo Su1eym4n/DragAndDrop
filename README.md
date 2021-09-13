a074a27c0257b62432a2e9b1f7514b776b2c258c
Sulaimanov, Suleiman <suleimansulaimanov@my.unt.edu>
2021-09-12 19:14:14 -0500

    Transparency and new gestures were added

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # DragAndDrop
 
 #About
-In this programm you will be able to drag and drop opjects you see, using your gestures.
+In this programm you will be able to drag, drop and resize opjects you see, using your gestures.
+
+To move objects with one hand, use your middle and tip fingers. 
+- If middle and tip fingers are far like in the `V sign` the user will go through the object without changing its state. 
+- If fingers are close and inside the object, the object will follow fingers' position.
+
+To move objects with both hands, use thumb and tip finger on both hands.
+- If hands are relaxed, nothing will change.
+- If thumb and tip fingers are positioned perpendicularly to each other on both hands, and the point between is inside the object, object will change its size and position.  
 
 ## Set up the environment
 Run following commands to set up the environment
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2
+import cvzone
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8)
@@ -7,39 +9,50 @@ detector = HandDetector(detectionCon=0.8)
 colorR = 255, 0, 255
 
 cx, cy, w, h = 800, 100, 200, 200
+startDist = None
+scale = 0
 
 class Rect():
 
-    def __init__(self, center, size=[200, 200], color=[255, 0, 255]):
+    def __init__(self, center, size=[200, 200], color=[255, 0, 255], isResizing=False):
         self.center = center
         self.size = size
-        self.color=color
+        self.color = color
+        self.isResizing = isResizing
 
     def update(self, cursor):
-
-        oldSize = self.size
-        oldColor = self.color
-
         cx, cy = self.center
         w, h = self.size
         if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
             self.center = cursor
             self.color = [0, 255, 0]
-            self.size = [220, 220]
+
+
+
+
+
+    def between(self,info):
+        w, h = self.size
+        cx, cy = self.center
+        if cx - w // 2 < info[0] < cx + w // 2 and cy - h // 2 < info[1] < cy + h // 2:
+            self.isResizing = True
+            self.center = info
         else:
-            self.size = oldSize
-            self.color = oldColor
+            self.isResizing = False
+
+    def leftBetween(self):
+        self.isResizing = False
+
+    def updateSize(self,scale):
+        w, h = self.size
+        if self.isResizing:
+            h = h + scale
+            w = w + scale
+        return [w, h]
 
 
 
 
-    def setView(self, newColor=[255, 0, 255], newSize = [200, 200]):
-        self.color = newColor
-        self.size = newSize
-
-
-
-rect = Rect([150, 150], [200, 200], colorR)
 
 listOfRects = []
 for x in range(5):
@@ -53,7 +66,7 @@ while True:
     # Find the hand and its landmarks
     hands, img = detector.findHands(img)  # with draw
 
-    if hands:
+    if len(hands) == 1:
         # Hand 1
         hand1 = hands[0]
         lmList1 = hand1["lmList"]  # List of 21 Landmark points
@@ -65,26 +78,58 @@ while True:
             for r in listOfRects:
                 r.update(cursor)
 
-        # else:
-        #     for r in listOfRects:
-        #         r.setView()
-        #     # rect.setView()
+
+
+    elif len(hands) == 2:
+        hand1 = hands[0]
+        hand2 = hands[1]
+
+        if detector.fingersUp(hand1) == [1, 1, 0, 0, 0] and detector.fingersUp(hand2) == [1, 1, 0, 0, 0]:
+            lmList1 = hand1["lmList"]  # List of 21 Landmark points
+            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+
+            length, info, img = detector.findDistance(lmList1[5], lmList2[5], img)
+
+
+            if startDist is None:
+                startDist = length
+            length, info, img = detector.findDistance(lmList1[5], lmList2[5], img)
+            scale = int((length - startDist)//2)
+            for r in listOfRects:
+                r.between(info[4:])
+            # info stores x, y center points
+            cx = info[4]
+            cy = info[5]
+            print(info)
+
+    else:
+        startDist = None
+        for r in listOfRects:
+            r.leftBetween()
 
 
 
+
+    #Draw transparent rect
+    imgNew = np.zeros_like(img, np.uint8)
     for r in listOfRects:
         cx, cy = r.center
         w, h = r.size
-        colorR = r.color
-        cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
 
-    # cx, cy = rect.center
-    # w, h = rect.size
-    # colorR = rect.color
-    # cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+        newWidth, newHeight = r.updateSize(scale)
+
+
+        colorR = r.color
+        cv2.rectangle(imgNew, (cx - newWidth // 2, cy - newHeight // 2), (cx + newWidth // 2, cy + newHeight // 2), colorR, cv2.FILLED)
+        cvzone.cornerRect(imgNew, (cx - w // 2, cy - h // 2, w, h), 20, rt=0)
+
+    out = img.copy()
+    alpha = 0.1
+    mask = imgNew.astype(bool)
+    out[mask] = cv2.addWeighted(img,alpha,imgNew,1-alpha,0)[mask]
 
 
     # Display
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", out)
     cv2.waitKey(1)
 
